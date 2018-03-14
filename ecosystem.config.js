153396520c2a0147d0b6b9b5c293722b24e4d3b1
ysfscream <894402575@qq.com>
2018-03-14 16:50:46 +0800

    feat: 视频上传接口 & 视频管理接口

---
+++ ecosystem.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+
+    // First application
+    {
+      name: 'www',
+      script: './bin/www',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      watch: true,
+      ignore_watch: [ // 从监控目录中排除
+        'public'
+      ],
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      user: 'node',
+      host: '212.83.163.1',
+      ref: 'origin/master',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev: {
+      user: 'node',
+      host: '212.83.163.1',
+      ref: 'origin/master',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/development',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env: {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+}
