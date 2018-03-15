9ead463d35e30843b00ede7e27224762f145d37c
ysfscream <894402575@qq.com>
2018-03-15 19:01:01 +0800

    feat: 添加数组分页

---
+++ middlewares/pagenation.js
@@ -0,0 +1,10 @@
+// 对数组进行分页
+const pageNation = (page, pageSize, array) => {
+  const offset = (page - 1) * pageSize
+  return ((offset + pageSize)  >= array.length) 
+           ? array.slice(offset, array.length)
+            : array.slice(offset, offset + pageSize)
+}
+  
+module.exports = pageNation
+
